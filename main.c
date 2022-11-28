433547b2364bb55f94da1bf15511934de14e09f5
luizgustavoou <luizgustavooumbelino@gmail.com>
2022-11-27 22:55:46 -0300

    feature: refatorar codigo em funcoes

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 /** Constantes para as strings a serem lidas */
 #define FIRST_SIZE_MY_HAND 7;
 const int SIZE_CARD = 5;
+const int SIZE_NAIPE = 4;
 #define MAX_LINE 100
 #define MAX_ACTION 10
 #define MAX_ID_SIZE 10
@@ -27,7 +28,7 @@ void allocMyHand(char ***new_my_hand, int hand_size) {
   }
 }
 
-void reallocMyHand(char ***new_my_hand, int new_hand_size) {
+void reallocMyHand(char ***new_my_hand, int new_hand_size){
   *new_my_hand = (char**) realloc(*new_my_hand, sizeof(char*)*(new_hand_size));
   for(int j=0; j<new_hand_size; j++) {
     (*new_my_hand)[j] = (char*) realloc((*new_my_hand)[j], sizeof(char)*SIZE_CARD);
@@ -52,6 +53,7 @@ char** removeCard(char **matriz, int size_matriz, int indice){
   return my_new_hand;
 }
 
+
 void setMyHand(char temp[MAX_LINE], char **my_hand) {
   int cont = 0;
   sscanf(temp, "[ %[^]] ]", temp);
@@ -66,6 +68,29 @@ void setMyHand(char temp[MAX_LINE], char **my_hand) {
   }
 }
 
+/* Funções de ações do bot */
+void actionDiscard(char ***my_hand, int *hand_size, int index_card) {
+  printf("DISCARD %s\n", (*my_hand)[index_card]);
+  *hand_size = *hand_size - 1;
+  reallocMyHand(my_hand, (*hand_size));
+  *my_hand = removeCard(*my_hand, *hand_size, index_card);
+
+  //strcpy(card_on_the_table, my_hand[c]);
+  
+}
+
+void actionBuy(char ***my_hand, int quant, int *hand_size){
+  char card[SIZE_CARD];
+  printf("BUY %d\n", quant);
+  for(int c = 0; c < quant; c++){
+    scanf("%s\n", card);
+    *hand_size = *hand_size + 1;
+    reallocMyHand(my_hand, (*hand_size));
+    strcpy((*my_hand)[*hand_size-1], card);
+  }
+}
+
+/* FIM Funções de ações do bot  */
 int main()
 {
   char temp[MAX_LINE];     // string para leitura temporária de dados
@@ -78,9 +103,9 @@ int main()
   allocMyHand(&my_hand, hand_size);
 
   char card_on_the_table[SIZE_CARD];
-  int discart = 0; //Para saber se discartou ou não para saber se irá comprar uma carta
+  int discard = 0; //Para saber se discardou ou não para saber se irá comprar uma carta
   int get_four_or_two = 0;
-   char naipe[100];
+  char naipe[SIZE_NAIPE];
 
   setbuf(stdin, NULL);  
   setbuf(stdout, NULL); 
@@ -98,13 +123,12 @@ int main()
 
   setMyHand(temp, my_hand);
 
-  //debugMyHand(my_hand, hand_size);
 
   scanf("TABLE %s\n", temp);
   
   strcpy(card_on_the_table, temp);
-  
-  //debug(card_on_the_table);
+
+
 
   // === PARTIDA ===
 
@@ -124,55 +148,28 @@ int main()
     while (strcmp(action, "TURN") || strcmp(complement, my_id));
 
     /* Início das ações do bot main */
-    
-    discart = 0;
+  
+    discard = 0;
 
     strncpy(naipe, card_on_the_table+1, 3);
-      for(int c = 0; c < hand_size; c++){
-        if(my_hand[c][0] == card_on_the_table[0]){
-          //caso pra letra igual
-          printf("DISCARD %s\n", my_hand[c]);
 
-          reallocMyHand(&my_hand, hand_size-1);
-          
-          my_hand = removeCard(my_hand, hand_size, c);
-          hand_size --;
-          //debugMyHand(my_hand, hand_size);
-          discart = 1;
-          //strcpy(card_on_the_table, my_hand[c]);
-          //debug(card_on_the_table);
-          break;
-        }
-        else if(strcmp(naipe, my_hand[c]+1) == 0){
-          //caso pra naipe igual
-          printf("DISCARD %s\n", my_hand[c]);
-          
-          reallocMyHand(&my_hand, hand_size-1);
-          
-          my_hand = removeCard(my_hand, hand_size, c);
-          hand_size --;
-          //debugMyHand(my_hand, hand_size);
-          discart = 1;
-          //strcpy(card_on_the_table, my_hand[c]);
-          //debug(card_on_the_table);
-          break;
-        }
+    for(int c = 0; c < hand_size; c++){
+
+      if(my_hand[c][0] == card_on_the_table[0] || strcmp(naipe, my_hand[c]+1) == 0){
+        //Verifica se tem letra/numero ou naipes correspondente
+        actionDiscard(&my_hand, &hand_size, c);
+
+        discard = 1;
+        break;
       }
-      
-    if(discart == 0){
-      char card[SIZE_CARD];
-      printf("BUY %d\n", 1);
-      scanf("%s\n", card);
-      hand_size ++;
-      //teste
-      my_hand = (char**) realloc(my_hand, sizeof(char*)*(hand_size));
-          for(int j=0; j<hand_size; j++) {
-            my_hand[j] = (char*) realloc(my_hand[j], sizeof(char)*SIZE_CARD);
-      }
-      strcpy(my_hand[hand_size-1], card);
-      //debugMyHand(my_hand, hand_size);
     }
-  }
 
-  return 0;
+    debugMyHand(my_hand, hand_size);
+
+    if(discard == 0){
+      actionBuy(&my_hand, 1, &hand_size);
+      }
+
+  }
+    return 0;
 }
